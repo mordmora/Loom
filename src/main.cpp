7974c1f0537f324e6929f8edf17f9c8cb8ff6944
mordmora <danielbonnett20@gmail.com>
2026-03-04 00:57:17 -0500

    fix arena

--- src/main.cpp
+++ src/main.cpp
@@ -13,21 +13,21 @@ int main(int argc, char** argv){
         return 1;
     }
 
-    umbra::SourceManager s_manager;
+    loom::SourceManager s_manager;
     uint16_t f = s_manager.loadFile(argv[1]);
-    umbra::SourceFileView v(s_manager.getView(f));
+    loom::SourceFileView v(s_manager.getView(f));
     Str s(static_cast<const uint8_t*>(v.buff), v.size);
-    umbra::Lexer l(s);
+    loom::Lexer l(s);
 
     memory::PoolAllocator _allocator(4096);
-    memory::STLAllocator<umbra::Token> token_alloc(_allocator);
+    memory::STLAllocator<loom::Token> token_alloc(_allocator);
 
-    std::vector<umbra::Token, memory::STLAllocator<umbra::Token>> tokens(token_alloc);
+    std::vector<loom::Token, memory::STLAllocator<loom::Token>> tokens(token_alloc);
     tokens.reserve(256);
 
     while (true) {
-        umbra::Token tk = l.getToken();
-        if (tk.type == umbra::TokenType::Eof || tk.type == umbra::TokenType::Inv)
+        loom::Token tk = l.getToken();
+        if (tk.type == loom::TokenType::Eof || tk.type == loom::TokenType::Inv)
             break;
         tokens.push_back(tk);
     }
